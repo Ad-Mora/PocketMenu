e3c69116be5407af9519a49a8674f688957d8b5e
Devin Morgan <devinm@mit.edu>
2016-06-09 22:56:39 -0400

    Made first draft of sliding underbar with in FilterSearchBar.js

--- PhotoMenu/static/PhotoMenu/JS/FilterSearchBar.js
+++ PhotoMenu/static/PhotoMenu/JS/FilterSearchBar.js
@@ -6,32 +6,48 @@
 var moreOptions = (function(){
 
     // cache DOM
-    var moreOptionsButton = document.querySelector("li.filter-option.more-options-options");
-    var moreOptionsList = moreOptionsButton.querySelector("ul.more-filter-options-list");
-    var dropDownIcon = moreOptionsButton.querySelector("i.more-options-drop-down-icon");
+    var visibleOptionsUL = document.querySelector("ul.filter-options-list");
+    var allVisibleOptionLI = visibleOptionsUL.querySelectorAll("li.filter-option");
+    var moreOptionsLI = visibleOptionsUL.querySelector("li.filter-option.more-options-options");
+    var moreOptionsUL = moreOptionsLI.querySelector("ul.more-filter-options-list");
+    var dropDownIcon = moreOptionsLI.querySelector("i.more-options-drop-down-icon");
+    var slidingUnderBar = visibleOptionsUL.querySelector("hr.sliding-underbar");
 
     // bind events
-    moreOptionsButton.addEventListener('click', toggleDropDownMenu);
+    moreOptionsLI.addEventListener('click', toggleDropDownMenu);
+    for (var i = 0; i < allVisibleOptionLI.length; i++) {
+        allVisibleOptionLI[i].addEventListener('click', adjustSlidingUnderBar);
+    }
 
     // private variables
     var dropDownIsVisible = false;
     var dropDownMenuStates = {true: "block", false: "none"};
     var dropDownIconStates = {true: "rotate(180deg)", false: "rotate(0deg)"};
+    var referenceX = visibleOptionsUL.getBoundingClientRect().left;
 
     // private methods
+    function toggleDropDownMenu(event) {
+        dropDownIsVisible = !dropDownIsVisible;
+        moreOptionsUL.style.display = dropDownMenuStates[ dropDownIsVisible ];
+        dropDownIcon.style.transform = dropDownIconStates[ dropDownIsVisible ];
+    }
+
+    function adjustSlidingUnderBar(event) {
+        var optionRect = this.getBoundingClientRect();
+        var deltaX = optionRect.left - referenceX;
+        var width = optionRect.width;
+
+        slidingUnderBar.style.width = width + "px";
+        slidingUnderBar.style.marginLeft = deltaX + "px";
+    }
 
     // public variables
 
     // public methods
-    function toggleDropDownMenu(event) {
-        dropDownIsVisible = !dropDownIsVisible;
-        moreOptionsList.style.display = dropDownMenuStates[ dropDownIsVisible ];
-        dropDownIcon.style.transform = dropDownIconStates[ dropDownIsVisible ];
-    }
 
     // reveal public pointers to private functions & properties
     return {
-        toggleDropDownMenu: toggleDropDownMenu
+
     };
 
 })();
