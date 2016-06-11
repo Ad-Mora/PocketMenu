42d0a172c0dece7a80ffde9e335b2ef42ff2836f
Devin Morgan <devinm@mit.edu>
2016-06-10 21:59:51 -0400

    Can navigate to next and previous foods from inspector view in Desktop (only) now

--- PhotoMenu/static/PhotoMenu/JS/AllInspectorView.js
+++ PhotoMenu/static/PhotoMenu/JS/AllInspectorView.js
@@ -11,37 +11,58 @@ var inspectorView = (function () {
     var desktopBackgroundOveraly = allInspectorsWrapper.querySelector("div.desktop-inspector-background-overlay");
     var desktopInspector = allInspectorsWrapper.querySelector("div.food-modal-container");
     var exitDesktopInspectorIcon = desktopInspector.querySelector("span.exit-food-modal-icon");
-    var desktopImageWrapper = desktopInspector.querySelector("div.food-modal-image-wrapper");
+    var rightPictureIcon = desktopInspector.querySelector("span.food-modal-right-arrow-icon");
+    var leftPictureIcon = desktopInspector.querySelector("span.food-modal-left-arrow-icon");
 
     // bind events
     exitMobileInspectorIcon.addEventListener('click', _hideInspectorView);
     mobileBackgroundOverlay.addEventListener('click', _hideInspectorView);
     exitDesktopInspectorIcon.addEventListener('click', _hideInspectorView);
     desktopBackgroundOveraly.addEventListener('click', _hideInspectorView);
-    desktopImageWrapper.addEventListener('mouseover', _highlightNavArrows);
+    rightPictureIcon.addEventListener('click', _showNextFood);
+    leftPictureIcon.addEventListener('click', _showPreviousFood);
 
     // private variables
+    var currentFood;
 
     // public variables
 
     // private functions
-    function _showInspectorView(dataDict) {
-        allInspectorsWrapper.style.display = "block";
-        /* populate inspector view with info from dataDict */
-    }
-
     function _hideInspectorView(event) {
         allInspectorsWrapper.style.display = "none";
     }
 
-    function _highlightNavArrows(event) {
-        console.log(event);
+    function _showNextFood() {
+        var nextFood = foodEntry.getNextFoodItem(currentFood);
+        populateMobileAndDesktopInspectorWithFood(nextFood);
     }
+
+    function _showPreviousFood() {
+        var previousFood = foodEntry.getPreviousFoodItem(currentFood);
+        populateMobileAndDesktopInspectorWithFood(previousFood);
+    }
+
+    function populateMobileAndDesktopInspectorWithFood(food) {
+        currentFood = food;
+        var foodName = currentFood.getAttribute("data-food-name");
+        var foodDetails = currentFood.getAttribute("data-food-details");
+        var foodImageLocaction = currentFood.getAttribute("data-food-image-location");
+        var foodPrice = currentFood.getAttribute("data-food-price");
+        var foodIsLiked = currentFood.getAttribute("data-food-is-liked");
+        var listItemNumber = currentFood.getAttribute("data-list-item-number");
+
+        console.log(listItemNumber);
+    }
+
     // public functions
+    function showInspectorView(clickedFood) {
+        populateMobileAndDesktopInspectorWithFood(clickedFood);
+        allInspectorsWrapper.style.display = "block";
+    }
 
     // return public pointers to private variables & functions
     return {
-        showInspectorView: _showInspectorView
+        showInspectorView: showInspectorView
     };
 
 })();
--- PhotoMenu/static/PhotoMenu/JS/FoodEntry.js
+++ PhotoMenu/static/PhotoMenu/JS/FoodEntry.js
@@ -2,12 +2,14 @@
  * Created by devinm on 6/9/16.
  */
 var foodEntry = (function () {
+
     // cache DOM
     var listOfFoodEntryLI = document.querySelectorAll("li.food-entry-li");
 
     // bind events
     for (var i = 0; i < listOfFoodEntryLI.length; i++) {
         listOfFoodEntryLI[i].addEventListener('click', _displayInspectorViewForFood);
+        listOfFoodEntryLI[i].setAttribute("data-list-item-number", i);
     }
 
     // private variables
@@ -16,26 +18,34 @@ var foodEntry = (function () {
 
     // private functions
     function _displayInspectorViewForFood(event) {
-        var foodName = this.getAttribute("data-food-name");
-        var foodDetails = this.getAttribute("data-food-details");
-        var foodImageLocaction = this.getAttribute("data-food-image-location");
-        var foodPrice = this.getAttribute("data-food-price");
-        var foodIsLiked = this.getAttribute("data-food-is-liked");
-
-        inspectorView.showInspectorView({
-            "foodName": foodName,
-            "foodDetails": foodDetails,
-            "foodImageLocation": foodImageLocaction,
-            "foodPrice": foodPrice,
-            "foodIsLiked": foodIsLiked
-        });
+        inspectorView.showInspectorView(this);
     }
 
     // public functions
+    function getNextFoodItem(foodItem) {
+        var currentFoodIndex = foodItem.getAttribute("data-list-item-number");
+        var nextFoodIndex = ++currentFoodIndex;
+        //if its the last element in the list, return the 1st element
+        if (nextFoodIndex == listOfFoodEntryLI.length) {
+            return listOfFoodEntryLI[0];
+        }
+        return listOfFoodEntryLI[ nextFoodIndex ];
+    }
+
+    function getPreviousFoodItem(foodItem) {
+        var currentFoodIndex = foodItem.getAttribute("data-list-item-number");
+        var previousFoodIndex = --currentFoodIndex;
+        // if its the first element in the list, return the last element
+        if (previousFoodIndex == -1) {
+            return listOfFoodEntryLI[ listOfFoodEntryLI.length -1 ];
+        }
+        return listOfFoodEntryLI[ previousFoodIndex ];
+    }
 
     // return public points to private variables and functions
     return {
-
+        "getNextFoodItem": getNextFoodItem,
+        "getPreviousFoodItem": getPreviousFoodItem
     };
 
 })();
