96697a732178bfe123e076f4f802094521894237
Adrian Mora <adriandariom@gmail.com>
2016-06-15 19:45:07 -0400

    Search suggestions hide and appear based on input and blur

--- PhotoMenu/static/PhotoMenu/JS/Homepage.js
+++ PhotoMenu/static/PhotoMenu/JS/Homepage.js
@@ -3,9 +3,17 @@ var searchDropDown = (function(){
     // cache DOM objects
     var darkOverlay = document.querySelector(".dark-overlay");
     var searchBar = darkOverlay.querySelector(".search-bar");
-    var searchSuggestions = darkOverlay.querySelector(".search-suggestions-wrapper");
+    var searchSuggestions = darkOverlay.querySelector(".search-suggestions-box");
 
     // bind events
-    searchBar.addEventListener("click", toggleSearchDropDown);
+    searchBar.addEventListener("input", toggleSearchDropDown);
+    searchBar.addEventListener("blur", toggleSearchDropDown);
+
+    function toggleSearchDropDown(event) {
+        searchSuggestions.style.display = "block";
+        if (searchBar.value === "" || event.type == "blur") {
+            searchSuggestions.style.display = "none";
+        }
+    }
 
 })();
