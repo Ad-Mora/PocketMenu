ca43d6e994423ce8cf280b521a658116d27fc966
Adrian Mora <adriandariom@gmail.com>
2016-06-15 15:59:42 -0400

    Began the javascript file for the search drop down

--- PhotoMenu/static/PhotoMenu/JS/Homepage.js
+++ PhotoMenu/static/PhotoMenu/JS/Homepage.js
@@ -1,3 +1,11 @@
-var moreOptions = (function(){
+var searchDropDown = (function(){
+
+    // cache DOM objects
+    var darkOverlay = document.querySelector(".dark-overlay");
+    var searchBar = darkOverlay.querySelector(".search-bar");
+    var searchSuggestions = darkOverlay.querySelector(".search-suggestions-wrapper");
+
+    // bind events
+    searchBar.addEventListener("click", toggleSearchDropDown);
 
 })();
