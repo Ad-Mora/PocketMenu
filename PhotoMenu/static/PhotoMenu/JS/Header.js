db5095ffebd19f19eeb9be98920df825fac7dea9
Devin Morgan <devinm@mit.edu>
2016-06-16 15:35:38 -0400

    Can now toggle the mobile-categories-menu via hamburger button and the dark overlay

--- PhotoMenu/static/PhotoMenu/JS/Header.js
+++ PhotoMenu/static/PhotoMenu/JS/Header.js
@@ -1,6 +1,39 @@
 /**
  * Created by devinm on 6/15/16.
  */
+var mobileCategoriesMenu = (function () {
+    // cache DOM
+    var menuWrapper = document.querySelector("div.mobile-categories-menu-wrapper");
+    var backgroundOverlay = menuWrapper.querySelector("div.categories-menu-background-overlay");
+    var categoriesList = menuWrapper.querySelector("ul.mobile-categories-menu-list");
+
+    // bind events
+    if (backgroundOverlay) {
+        backgroundOverlay.addEventListener('click', hideMobileCategoriesMenu);
+    }
+
+    // private variables
+
+    // public variables
+
+    // private functions
+    function hideMobileCategoriesMenu() {
+        menuWrapper.style.display = "none";
+    }
+
+    // public functions
+    function showMobileCategoriesMenu() {
+        menuWrapper.style.display = "block";
+    }
+
+    // return public pointers to private variables
+    return {
+        showMobileCategoriesMenu: showMobileCategoriesMenu
+    };
+
+})();
+
+
 var mobileSearchModal = (function(){
     // cached DOM
     var headerElement = document.querySelector("div.header");
@@ -13,7 +46,9 @@ var mobileSearchModal = (function(){
         searchIcon.addEventListener('click', mobileSearchModal.openSearchModal);
     }
     if (desktopSearchBar) {desktopSearchBar.addEventListener('click', function(){});}
-    if (hamburgerIcon) {hamburgerIcon.addEventListener('click', function(){});}
+    if (hamburgerIcon) {
+        hamburgerIcon.addEventListener('click', mobileCategoriesMenu.showMobileCategoriesMenu);
+    }
 
     // private variables
 
@@ -29,3 +64,4 @@ var mobileSearchModal = (function(){
     };
 
 })();
+
