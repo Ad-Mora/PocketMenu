96a61b4c8a468653ad19fc506399f8a72254279e
Adrian Mora <adriandariom@gmail.com>
2016-06-21 22:40:49 -0400

    Made description field in menu category non mandatory. Added an is header section boolean field to menu category

--- PhotoMenu/models.py
+++ PhotoMenu/models.py
@@ -51,7 +51,8 @@ class MenuCategory(models.Model):
 
     restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
+    is_header_section = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.restaurant.name + ' ' + self.name
