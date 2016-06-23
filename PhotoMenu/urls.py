2fc54b79109d89fb86ae8e088f9df068cff5edff
Adrian Mora <adriandariom@gmail.com>
2016-06-23 17:10:48 -0400

    Set up restaurant page so that context can now be passed into restuarant html page

--- PhotoMenu/urls.py
+++ PhotoMenu/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
     url(r'^contact/$', views.contact_page, name='contact_page'),
     url(r'^searchresults/$', views.search_results_page, name='search_results_page'),
-    url(r'^restaurantpage/$', views.restaurants_page, name='restaurant_homepage'),
     url(r'^favorites/$', views.favorites_page, name='favorites_page'),
+    url(r'^(?P<restaurant_name>[a-zA-Z]+(-[a-zA-z]+)*)/$', views.restaurants_page, name='restaurant_homepage'),
 ]
--- PhotoMenu/views.py
+++ PhotoMenu/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Restaurant
 
 # Create your views here.
 
@@ -12,8 +13,15 @@ def contact_page(request):
 def search_results_page(request):
     return render(request, 'PhotoMenu/SitePages/SearchResultsPage.html')
 
-def restaurants_page(request):
-    return render(request, 'PhotoMenu/SitePages/RestaurantPage.html')
+def restaurants_page(request, restaurant_name):
+
+    restaurant_name = restaurant_name.replace('-', ' ')
+    restaurant = Restaurant.objects.get(name=restaurant_name)
+
+    context = {
+        'restaurant': restaurant,
+    }
+    return render(request, 'PhotoMenu/SitePages/RestaurantPage.html', context)
 
 def favorites_page(request):
     return render(request, 'PhotoMenu/SitePages/FavoritesPage.html')
