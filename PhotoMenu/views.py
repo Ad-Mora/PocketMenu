51eef59a6366a7b10ffef56726e6a858546a57ef
Devin Morgan <devinm@mit.edu>
2016-06-23 12:09:59 -0400

    Made it so that whenever you submit the search form, your query will always reappear

--- PhotoMenu/views.py
+++ PhotoMenu/views.py
@@ -10,10 +10,24 @@ def contact_page(request):
     return render(request, 'PhotoMenu/SitePages/ContactPage.html')
 
 def search_results_page(request):
-    return render(request, 'PhotoMenu/SitePages/SearchResultsPage.html')
+    context = {}
+    print request.POST
+    try:
+        query_string = request.POST['search-bar']
+        print query_string
+        # search_for_food = request.POST['search-for-food']
+        #
+        context['query_string'] = query_string
+        # context['search_for_food'] = search_for_food
+
+    except (KeyError):
+        print "EXCEPTION!!!!!"
+        return render(request, 'PhotoMenu/SitePages/SearchResultsPage.html', context)
+
+    else:
+        return render(request, 'PhotoMenu/SitePages/SearchResultsPage.html', context)
 
 def restaurants_page(request,restaurant_name):
-    print restaurant_name
     return render(request, 'PhotoMenu/SitePages/RestaurantPage.html')
 
 def favorites_page(request):
