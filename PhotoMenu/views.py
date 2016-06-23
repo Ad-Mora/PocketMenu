012a249db60f5b39eea7c136fed817f21581eb59
Devin Morgan <devinm@mit.edu>
2016-06-23 18:26:04 -0400

    Created HTML and CSS for 'No Results' in SearchPage

--- PhotoMenu/views.py
+++ PhotoMenu/views.py
@@ -16,9 +16,12 @@ def search_results_page(request):
 
     if request.method == "POST":
         query_string = request.POST['search-bar']
-        food_matches = MenuItem.objects.filter(name__contains=query_string)
-        print("query_string: %s\nfood_mathches: %s" % (query_string, food_matches))
-        # search_for_food = request.POST['search-for-food']
+        food_matches = {}
+
+        if (query_string != ""):
+            food_matches = MenuItem.objects.filter(name__contains=query_string)
+            # print("query_string: %s\nfood_mathches: %s" % (query_string, food_matches))
+            # search_for_food = request.POST['search-for-food']
 
         context['query_string'] = query_string
         context['food_items'] = food_matches
