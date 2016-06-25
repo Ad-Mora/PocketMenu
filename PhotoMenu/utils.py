cf7223a4995616698625b05b189bc29006dfc803
Devin Morgan <devinm@mit.edu>
2016-06-24 21:03:57 -0400

    MasterSearchBar resturns links to the 'suggested' Restaurant's page and autocompletes for 'suggested' food items. The SearchResultsPage will only display foods

--- PhotoMenu/utils.py
+++ PhotoMenu/utils.py
@@ -1,14 +1,20 @@
-from .models import MenuItem
-import json
+from .models import MenuItem, Restaurant
+
 
 def get_menu_items_for_search_string(query_string, limit=30):
-    context = {}
-
     results = {}
     if query_string != "":
         results = MenuItem.objects.filter(name__contains=query_string)[:limit]
+    return results
 
-    context['query_string'] = query_string
-    context['menu_items_list'] = results
-    # context['search_for_food'] = search_for_food
-    return context
+
+def get_restaurants_for_search_string(query_string, limit=5):
+    restaurant_list = []
+    if query_string != "":
+        results = Restaurant.objects.filter(name__contains=query_string)[:limit]
+        for restaurant in results:
+            restaurant_list.append({
+                'name': restaurant.name,
+                'link': "../" + restaurant.name.replace(" ", "-")
+            })
+    return restaurant_list
--- PhotoMenu/views.py
+++ PhotoMenu/views.py
@@ -1,41 +1,58 @@
-# from django.core.context_processors import csrf
 from django.shortcuts import render
 from .utils import *
+import json
 
-# Create your views here.
 
 # AJAX
 def drop_down_suggestions(request):
-    query_string = ""
-
     if request.method == "POST":
         json_data = json.loads(request.body)
-        print json_data
+        search_type = json_data['search-type']
         query_string = json_data['search-bar']
 
-    context = get_menu_items_for_search_string(query_string, 5)
-    return render(request, 'PhotoMenu/Snippets/AutoCompleteSuggestions.html', context)
+        # either MenuItems or Restaurants can be 'suggested'
+        if search_type == "food":
+            context = {
+                'query_string':     query_string,
+                'menu_items_list':  get_menu_items_for_search_string(query_string, 5)
+            }
+            return render(request, 'PhotoMenu/Snippets/AutoCompleteFoodSuggestions.html', context)
+
+        elif search_type == "restaurant":
+            context = {
+                'query_string':     query_string,
+                'restaurants_list':  get_restaurants_for_search_string(query_string)
+            }
+            return render(request, 'PhotoMenu/Snippets/AutoCompleteRestaurantSuggestions.html', context)
+
 
 # SitePages
 def homepage(request):
     return render(request, 'PhotoMenu/SitePages/Homepage.html')
 
+
 def contact_page(request):
     return render(request, 'PhotoMenu/SitePages/ContactPage.html')
 
-def search_results_page(request):
 
+def search_results_page(request):
     if request.method == "POST":
         query_string = request.POST['search-bar']
-        print request.POST
-        context = get_menu_items_for_search_string(query_string)
-        print request.POST['search-type']
-        context['search_type'] = request.POST['search-type']
 
+        # only MenuItems can be returned to the SearchResultsPage (for now)
+        context = {
+            'query_string':     query_string,
+            'menu_items_list':  get_menu_items_for_search_string(query_string, 5),
+            'search_type':      request.POST['search-type']
+        }
         return render(request, 'PhotoMenu/SitePages/SearchResultsPage.html', context)
+    else:
+        return render(request, 'PhotoMenu/SitePages/SearchResultsPage.html')
+
 
 def restaurants_page(request,restaurant_name):
     return render(request, 'PhotoMenu/SitePages/RestaurantPage.html')
+
 
 def favorites_page(request):
     return render(request, 'PhotoMenu/SitePages/FavoritesPage.html')
