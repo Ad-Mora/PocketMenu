10bec2659f0aed05e2dae1e286a8665ce8dda9fc
Devin Morgan <devinm@mit.edu>
2016-06-29 15:45:19 -0400

    MasterSearchBar now uses an absolute path to reference its autocomplete suggestions

--- PhotoMenu/urls.py
+++ PhotoMenu/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
     url(r'^contact-us/$', views.contact_page, name='contact_page'),
     url(r'^search/$', views.search_results_page, name='search_results_page'),
-    url(r'^drop-down-suggestions/$', views.drop_down_suggestions),
+    url(r'^drop-down-suggestions/$', views.drop_down_suggestions, name='drop_down_suggestions'),
     url(r'^load-favorite-foods/$', views.get_favorite_foods),
     url(r'^my-favorites/$', views.favorites_page, name='favorites_page'),
     url(r'^(?P<restaurant_name>[a-zA-Z]+(-[a-zA-z]+)*)/search/drop-down-suggestions$',
