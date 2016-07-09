691960eeea60006bb46246f51a5e5b7924617947
Adrian Mora <adriandariom@gmail.com>
2016-07-08 21:55:45 -0400

    Put max character limits

--- PhotoMenu/forms.py
+++ PhotoMenu/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ContactUsForm(forms.Form):
-    name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'text-input-field'}))
-    email = forms.EmailField(required=False, widget=forms.TextInput(attrs={'class': 'text-input-field'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'message-area'}))
+    name = forms.CharField(required=False, max_length=100, widget=forms.TextInput(attrs={'class': 'text-input-field'}))
+    email = forms.EmailField(required=False, max_length=100, widget=forms.TextInput(attrs={'class': 'text-input-field'}))
+    message = forms.CharField(max_length=2000, widget=forms.Textarea(attrs={'class': 'message-area'}))
 
