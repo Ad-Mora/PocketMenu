8782618ff34156ba803d5e53c490b80615a88840
Devin Morgan <devinm@mit.edu>
2016-07-10 15:36:05 -0400

    Just made computer_restaurant_hours a function of the Restaurant model. Still working on trying to make the RestaurantEntry

--- PhotoMenu/views.py
+++ PhotoMenu/views.py
@@ -96,13 +96,19 @@ def contact_page(request):
 def search_results_page(request):
     if request.method == "POST":
         query_string = request.POST['search-bar']
-
-        # only MenuItems can be returned to the SearchResultsPage (for now)
-        context = {
-            'query_string':     query_string,
-            'menu_items_list':  get_menu_items_for_search_string(query_string),
-            'search_type':      request.POST['search-type']
-        }
+        context = None
+        if query_string == SEARCH_TYPE_FOOD:
+            context = {
+                'query_string': query_string,
+                'menu_items_list': get_menu_items_for_search_string(query_string),
+                'search_type': request.POST['search-type']
+            }
+        elif query_string == SEARCH_TYPE_RESTAURANT:
+            context = {
+                'query_string': query_string,
+                'restaurants_list': get_restaurants_for_search_string(query_string),
+                'search_type': request.POST['search-type']
+            }
         return render(request, 'PhotoMenu/SitePages/SearchResultsPage.html', context)
 
     else:
@@ -112,6 +118,7 @@ def search_results_page(request):
 def restaurants_page(request, restaurant_name):
     restaurant_name = restaurant_name.replace('-', ' ')
     restaurant = Restaurant.objects.get(name__iexact=restaurant_name)
+    restaurant.compute_store_hours()
     num_horizontal_cats = 2
     menu_categories = None
     categories = None
@@ -151,43 +158,6 @@ def restaurants_page(request, restaurant_name):
         for category in menu_categories:
             categories[category] = MenuItem.objects.filter(menu_category_id=category.id)
 
-    #----------------------------------------Should this go in utils.py ?
-    # get store hours
-    hours_grouping_list = [['Mon']]
-    time_groupings = [(restaurant.mon_open_time, restaurant.mon_close_time)]
-    num_groups = 0
-    store_hours = []
-
-    days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-
-    opening_times = [restaurant.mon_open_time, restaurant.tue_open_time,
-                     restaurant.wed_open_time, restaurant.thu_open_time,
-                     restaurant.fri_open_time, restaurant.sat_open_time,
-                     restaurant.sun_open_time]
-
-    closing_times = [restaurant.mon_close_time, restaurant.tue_close_time,
-                     restaurant.wed_close_time, restaurant.thu_close_time,
-                     restaurant.fri_close_time, restaurant.sat_close_time,
-                     restaurant.sun_close_time]
-
-    for i in range(len(days_of_week)-1):
-        if opening_times[i] == opening_times[i+1] and closing_times[i] == closing_times[i+1]:
-            hours_grouping_list[num_groups].append(days_of_week[i+1])
-        else:
-            hours_grouping_list.append([days_of_week[i+1]])
-            time_groupings.append((opening_times[i+1], closing_times[i+1]))
-            num_groups += 1
-
-    for i in range(len(hours_grouping_list)):
-        first_day_in_group = hours_grouping_list[i][0]
-        last_day_in_group = hours_grouping_list[i][-1]
-        days_string = first_day_in_group + '-' + last_day_in_group + ':'
-        if first_day_in_group == last_day_in_group:
-            days_string = first_day_in_group + ':'
-        store_hours.append((days_string, time_groupings[i][0], time_groupings[i][1]))
-
-    #--------------------------------------------
-
     horizontal_cats = menu_categories[:num_horizontal_cats]
     remaining_cats = menu_categories[num_horizontal_cats:]
     context = {
@@ -195,7 +165,6 @@ def restaurants_page(request, restaurant_name):
         'categories': categories,
         'horizontal_cats': horizontal_cats,
         'remaining_cats': remaining_cats,
-        'store_hours': store_hours,
         'search_options_list': [restaurant.name, SEARCH_TYPE_RESTAURANT, SEARCH_TYPE_FOOD],
         'is_restaurant_page': True
     }
